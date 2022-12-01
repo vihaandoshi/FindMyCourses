5b3f12c443ec0204deef0f07b214a5f82e263532
Vihaan Doshi <vihaan@berkeley.edu>
2022-11-30 18:04:08 -0800

    Added imports in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import React from "react";
 import Navbar from './Components/Navbar.jsx';
 import Footer from './Components/Footer.jsx';
 import {Routes, Route } from 'react-router-dom';
