928ee396bfb82773aa1674057fd37721c09a11b7
kellyyyeh <pegasusyeh@gmail.com>
2022-12-01 18:15:15 -0800

    changed questions to h3, added submit button, changed to arrow function

--- client/src/pages/Survey.js
+++ client/src/pages/Survey.js
@@ -1,29 +1,34 @@
 import React from "react";
-export default function Survey(){
+const Survey = () => {
     return (
         <div>
             <h1> Rate a class! </h1>
             <form className="survey-questions">
-                <h2>
+                <h3>
                     What class is this for?
                     {/* Should we use a dropdown? */}
                     <input type="text"/>
-                </h2>
-                <h2> Professor <input type="text"/> </h2>
-                <h2> Year <input type="text"/> </h2>
-                <h2> Semester <input type="text"/> </h2>
-                <h2> Difficulty <input type="text"/> </h2>
-                <h2> Grade Received <input type="text"/> </h2>
-                <h2> Required textbooks <input type="text"/> </h2>
-                <h2> Hours a week for homework <input type="text"/> </h2>
-                <h2> Number of midterms <input type="text"/> </h2>
-                <h2> Has a no homework option <input type="checkbox" value="true" /> </h2>
-                <h2> Has a clobber policy <input type="checkbox" value="true" /> </h2>
-                <h2> Has mandatory discussion attendance <input type="checkbox" value="true" /> </h2>
-                <h2> Prereqs/coreqs <input type="text"/> </h2>
-                <h2> Classes to help prepare <input type="text"/> </h2>
-                <h2> Next classes to take <input type="text"/> </h2>
-            </form>
-        </div>
+                </h3>
+                <h3> Professor <input type="text"/> </h3>
+                <h3> Year <input type="text"/> </h3>
+                <h3> Semester <input type="text"/> </h3>
+                <h3> Difficulty (1-10) <input type="number" min="1" max="10"/> </h3>
+                <h3> Grade Received <input type="text"/> </h3>
+                <h3> Required textbooks <input type="text"/> </h3>
+                <h3> Hours a week for homework <input type="text"/> </h3>
+                <h3> Number of midterms <input type="text"/> </h3>
+                <h3> Prereqs/coreqs <input type="text"/> </h3>
+                <h3> Classes to help prepare <input type="text"/> </h3>
+                <h3> Next classes to take <input type="text"/> </h3>
+                <h3> Has a no homework option <input type="checkbox" value="true" /> </h3>
+                <h3> Has a clobber policy <input type="checkbox" value="true" /> </h3>
+                <h3> Has mandatory discussion attendance <input type="checkbox" value="true" /> </h3>
+
+             <input type="submit"/>
+
+         </form>
+     </div>
     )
 }
+export default Survey
+
