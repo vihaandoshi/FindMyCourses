ddbeb3eb497d05d1a4233d476f3a7ad72fd7c548
Vihaan Doshi <vihaan@berkeley.edu>
2022-12-01 22:15:55 -0800

    Working on getting data from DB

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,28 @@
 require('dotenv').config
 
 const express = require('express')
-
 const app = express()
 
-app.listen(process.env.PORT, () => {
-    console.log("Listening");
-})
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://findcourses:fullstackdecal@findmycourses.abmjg6z.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log("Listening");
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+    
+const courseCardModel = require('./models/CourseCard')
+
+app.get("/getCourseCards", (req, res) => {
+    courseCardModel.find({}, (err, result) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(result);
+      }
+    });
+});
+
